efe39bb98622a93e0455715efd0778737625c056
aliyazıcı <93381581+aliyzc58@users.noreply.github.com>
2023-08-10 23:48:09 +0300

    Add files via upload

---
+++ LibraryUI/Controllers/CustomBaseController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Service.APIHandler;
+
+namespace LibraryUI.Controllers
+{
+    public class CustomBaseController : Controller
+    {
+        protected readonly IAPIHandler _apiHandler;
+        protected readonly IConfiguration _configuration;
+        protected readonly string _url;
+
+        public CustomBaseController(IAPIHandler apiHandler, IConfiguration configuration)
+        {
+            _apiHandler = apiHandler;
+            _configuration = configuration;
+            _url = this._configuration["BaseUrl"];
+        }
+
+     
+
+    }
+}
---
+++ LibraryUI/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Dto;
+using LibraryUI.FileUpload;
+using LibraryUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Service.APIHandler;
+using System.Diagnostics;
+
+namespace LibraryUI.Controllers
+{
+    public class HomeController : CustomBaseController
+    {
+        private readonly IFileUpload _fileUpload;
+        public HomeController(IAPIHandler apiHandler, IConfiguration configuration, IFileUpload fileUpload) : base(apiHandler, configuration)
+        {
+            _fileUpload = fileUpload;
+        }
+
+        public IActionResult Index()
+        {
+            List<BookListDto> listbook = _apiHandler.GetApi<List<BookListDto>>(_url + UrlStrings.GetBookList);
+            return View(listbook); 
+        }
+
+
+        [HttpGet]
+        public IActionResult AddBook()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult AddBook(BookCreateDto createDto,IFormFile formFile)
+        {
+            if(formFile != null) 
+            {
+                createDto.ImagePath = _fileUpload.FileUploads(formFile); 
+            }
+            var post = _apiHandler.PostApi<BookCreateDto>(createDto, _url + UrlStrings.AddBook);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Browwer(BrowwerDto dto)
+        {
+            var post = _apiHandler.PostApi<BookCreateDto>(dto, _url + UrlStrings.AddBrowwer);
+
+            return RedirectToAction("Index");
+        }
+
+       
+    }
+}
