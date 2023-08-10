21ac4765e4752ea9d4cdaf807423b7d540afb6be
aliyazıcı <93381581+aliyzc58@users.noreply.github.com>
2023-08-10 23:40:12 +0300

    Add files via upload

---
+++ LibraryAPI/Controllers/BookController.cs
@@ -0,0 +1,120 @@
+﻿using Core.Entity;
+using Dto;
+using Microsoft.AspNetCore.Mvc;
+using Service.IServices;
+
+namespace LibraryAPI.Controllers
+{
+    public class BookController : CustomBaseController
+    {
+        private readonly ILogger<BookController> _logger;
+        private readonly IBookService _bookService;
+        private readonly ILoanedBookService _loanedBookService;
+        public BookController(IBookService bookService, ILoanedBookService loanedBookService, ILogger<BookController> logger)
+        {
+            _bookService = bookService;
+            _loanedBookService = loanedBookService;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetBookList()
+        {
+            /*refoactoring
+             * join işlemi ile tek seferde kitaplar ödünç alan kişiyle birlikte çekilebilir
+               try catch işlemleri service katmanında yapılıp daha temiz bir kod elde edilebilirdi */
+            _logger.LogWarning(ErrorTypes.ListBook);
+            List<BookListDto> bookListDtos = new List<BookListDto>();
+            
+            try
+            {             
+         
+                List<Book> book = _bookService.GetAll();
+                foreach (var item in book)
+                {
+                    BookListDto bookListDto = new BookListDto();
+                    bookListDto.Id = item.Id;
+                    bookListDto.ImagePath=item.ImagePath;
+                    bookListDto.BookName=item.BookName;
+                    bookListDto.WriterName=item.WriterName;
+                    bookListDto.Status = item.Status;
+                    if (item.Status == false)
+                    {
+                        var loaned = _loanedBookService.GetBy(x => x.BookId == item.Id).FirstOrDefault();
+                        bookListDto.BrowwerDate = loaned.BrowwerDate;
+                        bookListDto.BrowwerName = loaned.BorrowerName;
+                    }
+                    bookListDtos.Add(bookListDto);   
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+            }
+
+            return Ok(bookListDtos);
+        }
+
+        //Kitap Ekleme İşlemi
+        [HttpPost]
+        public IActionResult AddBook(BookCreateDto createDto)
+        {
+            try
+            {
+                Book book = new Book()
+                {
+                    CreateDate = DateTime.Now,
+                    IsDeleted = false,
+                    BookName = createDto.BookName,
+                    WriterName = createDto.WriterName,
+                    Status = true,
+                    ImagePath = createDto.ImagePath,
+                };
+
+                _bookService.Add(book);
+                _logger.LogWarning(createDto.BookName + ErrorTypes.AddBook);
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+            }
+
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult AddBrowwer(BrowwerDto createDto)
+        {
+            //refactoring 
+            /* databaseden kitabın olup olmadığı sorgulanabilir
+                   //throw new KitapBulunamadı(); */
+            try
+            {
+                //Ödünç Alan Kişi Oluşturuluyor
+                LoanedBook browwer = new LoanedBook()
+                {
+                    BookId = createDto.BookId,
+                    BrowwerDate = createDto.BrowwerDateTime,
+                    BorrowerName = createDto.BrowwerName + createDto.BrowwerSurname,
+
+                };
+                _loanedBookService.Add(browwer);
+                
+                //Kitabın kütüphanedeki durumu güncelleniyor
+                var book = _bookService.GetById(createDto.BookId);
+                book.Status = false;
+                book.UpdateDate = DateTime.Now;
+                _bookService.Update(book);
+                _logger.LogWarning(createDto.BrowwerName + " adlı kişiye kitap emanet edildi");
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+            }
+
+            return Ok();
+        }
+
+    }
+}
---
+++ LibraryAPI/Controllers/CustomBaseController.cs
@@ -0,0 +1,55 @@
+﻿using Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CustomBaseController : ControllerBase
+    {
+
+        public CustomBaseController()
+        {
+        }
+        
+        public enum HttpStatusCode
+        {
+            NotFound = 404,
+            BadRequest = 404,
+            UnSporttedMediaType = 415,
+            Created = 201,
+            NoContent = 204,
+            Ok = 200
+
+        }
+
+        public enum EntityStatus
+        {
+            Passive = 2,
+            Active = 1,
+            AwaitingResponseDiveCenter = 0
+        }
+       
+        public class ErrorTypes
+        {
+            public static string AddBook = " adlı kitap eklendi";
+            public static string ListBook = "Kitap liste sayfası açıldı";
+
+        }
+        [NonAction]
+        public IActionResult CreateActionResult<T>(CustomResponseDto response)
+        {
+            if (response.StatusCode == 204)
+                return new ObjectResult(null)
+                {
+                    StatusCode = response.StatusCode
+                };
+
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+
+    }
+}
